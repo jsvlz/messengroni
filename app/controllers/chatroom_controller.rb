f158a040f492b4388ce52e3af11025d0089c6a6e
Jose Veliz <jveliz3821@gmail.com>
2019-09-03 22:42:18 -0400

    initial commit

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,13 @@
+class ChatroomController < ApplicationController
+  before_action :require_user, only: :index
+
+  def index
+    @messages = Message.custom_display
+    @message = Message.new
+  end
+
+  def create
+
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+#Use this plus $rails db:seed (in compiler) to populate db
+# User.create(username: "Joanna", password: "password")
+# User.create(username: "Rusty", password: "password")
+# User.create(username: "Bianca", password: "password")
+# User.create(username: "Biker", password: "password")
+# User.create(username: "JoeDirt", password: "password")
+# User.create(username: "Jeep", password: "password")
